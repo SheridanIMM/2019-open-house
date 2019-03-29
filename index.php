bed2eb1ebc97028fddd6ea3a8d88c53b9e9f279b
KateLauren93 <kate-carson@outlook.com>
2019-03-29 17:22:48 -0400

    Initial HTML and CSS started for site

---
+++ index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <!-- Font Awesome for icons -->
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+        <!-- Roboto font -->
+        <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+        <!-- Our custom CSS -->
+        <link rel="stylesheet" href="http://localhost/webdev/2019-open-house/css/main.css">
+        <title>IMM 2019 Grad Show</title>
+    </head>
+    <body>
+        <div class="container top-banner">
+            <div class="row text-center">
+                <div class="col-sm-12">
+                    <h1 class="title">Innovative</h1>
+                </div>
+            </div>
+            <div class="row text-center">
+                <div class="col-sm-12">
+                    <h2 class="subtitle">IMM 2019 Grad Show</h2>
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 col-sm-12">
+                    <p><i class="fas fa-graduation-cap icon"></i>Sheridan College</p>
+                    <p><i class="fas fa-calendar-alt icon"></i>May 9th from 6 - 9 PM</p>
+                    <p><i class="fas fa-map-marker-alt icon"></i>Hosted by Jam3</p>
+                    <p>325 Adelaide Street West, Toronto</p>
+                </div>
+                <div class="col-lg-8 col-sm-12">
+                    <h3>What is IMM?</h3>
+                    <p>The Interactive Multimedia Management program from Sheridan College is a creative, technical, and fast-paced program. The students cover topics ranging from full-stack web development, to user interface / user experience, digital media, and project management. Join the graduating class this May for drinks, food, and to see demonstrations of the wonderful work achieved this year!</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    </body>
+</html>
