25d896c229569ca53e22c9c6bbbf527faca180b7
NgocTo <baongoc1308@gmail.com>
2019-03-29 16:31:07 -0400

    structure

---
+++ content.php
@@ -0,0 +1,3 @@
+<!-- Put your main content here -->
+<p>This is the main content</p>
+<img src="img/placeholder.png" alt="placeholder">
---
+++ nav.php
@@ -0,0 +1,7 @@
+<!-- Put your navigation here -->
+<p>This is the navigation (hamburger menu/sidebar)</p>
+<ul>
+    <li class="nav-link"><a href="#"><i class="fas fa-home"></i><span>Home<span></a></li>
+    <li class="nav-link"><a href="#"><i class="fas fa-users"></i><span>Student work<span></a></li>
+    <li class="nav-link"><a href="#"><i class="fas fa-map-marker-alt"></i><span>Location<span></a></li>
+</ul>
