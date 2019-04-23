51f52a77d9dff32dde03365e9563434bbb43dffa
Jaime <jaimeconvery@gmail.com>
2019-04-22 23:27:33 -0400

    fixed css

--- nav.php
+++ nav.php
@@ -1,92 +1,29 @@
-<!-- Put your navigation here -->
-<!-- No need to put the link, script and head meta tags here -->
-<!-- <p>Navigation (coming soon)</p> -->
-<!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
-   <a class="navbar-brand" href="#">Navbar</a>
-   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-     <span class="navbar-toggler-icon"></span>
-   </button>
-   <div class="collapse navbar-collapse" id="navbarNav">
-     <ul class="navbar-nav">
-       <li class="nav-item active">
-         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-       </li>
-       <li class="nav-item">
-         <a class="nav-link" href="#">Features</a>
-       </li>
-       <li class="nav-item">
-         <a class="nav-link" href="#">Pricing</a>
-       </li>
-       <li class="nav-item">
-         <a class="nav-link disabled" href="#">Disabled</a>
-       </li>
-     </ul>
-   </div>
- </nav> -->
-<?php?>
-<!-- <!DOCTYPE html>
-<html lang="en">
+<? php ?>
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <!-- Bootstrap CSS -->
-    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <!-- Font Awesome for icons -->
-    <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css"
-        integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
-    <!-- Roboto font -->
-    <!-- <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
-    <!-- Our custom CSS -->
-    <!-- <link rel="stylesheet" href="css/main.css">
-    <title>IMM 2019 Grad Show</title>
-</head>
+<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+</button>
+<div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <button id="closeBtn" class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">&times;</button>
+    <ul>
+        <li>
+            <a class="nav-link" href="#">
+                <i class="fas fa-home"></i>
+            </a>
+        </li>
+        <li>
+            <a class="nav-link" href="#">
+                <i class="fas fa-users"></i>
+            </a>
+        </li>
+        <li>
+            <a class="nav-link" href="#">
+                <i class="fas fa-map-marker-alt"></i>
+            </a>
+        </li>
+    </ul>
 
-<body>
-    <p>Navigation (coming soon)</p> --> --> --> --> -->
-    <!-- <nav class="navbar navbar-expand-lg navbar-dark"> -->
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <button id="closeBtn" class="navbar-toggler" type="button" data-toggle="collapse"
-                data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">&times;</button>
-            <ul>
-                <li>
-                    <a class="nav-link" href="#">
-                        <i class="fas fa-home"></i>
-                    </a>
-                </li>
-                <li>
-                    <a class="nav-link" href="#">
-                        <i class="fas fa-users"></i>
-                    </a>
-                </li>
-                <li>
-                    <a class="nav-link" href="#">
-                        <i class="fas fa-map-marker-alt"></i>
-                    </a>
-                </li>
-            </ul>
+</div>
 
-        </div>
-    <!-- </nav> -->
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <!-- <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-        crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-        crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-        crossorigin="anonymous"></script> -->
-<!-- </body>
 
-</html> -->
-
-<?php?>
+<? php ?>
