50f610f196c6dbcd7650c8642022f96e55e979bc
NgocTo <baongoc1308@gmail.com>
2019-04-30 14:43:51 -0400

    fixed map

--- js/map.js
+++ js/map.js
@@ -1,27 +1,31 @@
 console.log("mapjs");
 var map, infoWindow;
-function initMap() {
-    var lat = 43.647180;
-    var lng = -79.392550;
-    var pos = {
-        lat: lat,
-        lng: lng
-    };
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: lat, lng: lng},
-        zoom: 14
-    });
+function initMap() {} // now it IS a function and it is in global
 
-    var marker = new google.maps.Marker({
-        position: pos,
-        map: map,
-        title: 'Sheridan IMM Open House'
-    });
-    var content = 'Sheridan IMM Open House, 325 Adelaide Street West, Toronto';
-    infoWindow = new google.maps.InfoWindow({
-        content: content
-    });
-    marker.addListener('click', function() {
-        infoWindow.open(map, marker);
-    });
-}
+$(() => {
+    initMap = function() {
+        var lat = 43.647180;
+        var lng = -79.392550;
+        var pos = {
+            lat: lat,
+            lng: lng
+        };
+        map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: lat, lng: lng},
+            zoom: 14
+        });
+
+        var marker = new google.maps.Marker({
+            position: pos,
+            map: map,
+            title: 'Sheridan IMM Open House'
+        });
+        var content = 'Sheridan IMM Open House, 325 Adelaide Street West, Toronto';
+        infoWindow = new google.maps.InfoWindow({
+            content: content
+        });
+        marker.addListener('click', function() {
+            infoWindow.open(map, marker);
+        });
+    }
+})
