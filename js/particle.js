8c2c84f53418b239d8a26a87a87afb7c63f4b482
Deep Kamani <deep.kamani24@gmail.com>
2019-05-04 02:26:39 +0530

    density increased from 100- 130

--- js/particle.js
+++ js/particle.js
@@ -1,7 +1,7 @@
 particlesJS("particles-js", {
     "particles": {
       "number": {
-        "value": 100,
+        "value": 130,
         "density": {
           "enable": true,
           "value_area": 500
